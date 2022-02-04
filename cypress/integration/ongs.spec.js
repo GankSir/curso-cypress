f6077adc09c05621227ffdc3ce4f9085d43bc20f
GankSir <sirgank13@gmail.com>
2022-02-04 12:37:15 -0300

    inicio

---
+++ cypress/integration/ongs.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Ongs', () => {
+    it('devem poder realizar um cadastro', () => {
+
+    });
+
+    it('deve realizar um login no sistema', () => {
+
+    });
+
+});
