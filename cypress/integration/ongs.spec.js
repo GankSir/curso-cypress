86793075d9abd91f55ce75c3b85ed2f2f3e1a637
GankSir <sirgank13@gmail.com>
2022-02-07 11:07:49 -0300

    adjusted

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -2,7 +2,17 @@
 
 describe('Ongs', () => {
     it('devem poder realizar um cadastro', () => {
-
+        //cy.visit enter app location
+        cy.visit('http://localhost:3000/register')
+        //cy.get search for an element 
+        //.type insert an element
+        cy.get('[data-cy=name]').type('bruno');
+        cy.get('[data-cy=email]').type('teste@teste.com');
+        cy.get('[data-cy=whatsapp]').type('31991442623');
+        cy.get('[data-cy=city]').type('Sete Lagoas');
+        cy.get('[data-cy=uf]').type('MG');
+        //click button
+        cy.get('[data-cy=submit]').click()
     });
 
     it('deve realizar um login no sistema', () => {
